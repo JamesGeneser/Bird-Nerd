2b6bb5273dd701a019b93a8e4b96e7c412f91d86
James Geneser <jamesgeneser@gmail.com>
2023-04-18 13:52:24 -0600

    App.js file

--- client/src/App.js
+++ client/src/App.js
@@ -1,1 +1,32 @@
 import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import Home from "./pages/Home";
+import IdentifyBird from "./pages/IdentifyBird";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import PostSighting from "./pages/PostSighting";
+
+const client = new ApolloClient({
+  uri: "/graphql",
+  cache: new InMemoryCache(),
+});
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/identify" element={<IdentifyBird />} />
+          <Route path="/post" element={<PostSighting />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </ApolloProvider>
+  );
+}
