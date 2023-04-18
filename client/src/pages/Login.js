445d0c28d6bcf1b851aa35d6da7d55367976f482
James Geneser <jamesgeneser@gmail.com>
2023-04-18 13:16:47 -0600

    jsx code in login

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -36,4 +36,50 @@ const Login = (props) => {
       password: "",
     });
   };
+
+  return (
+    <main>
+      <div className="card">
+        <h4>Login</h4>
+        <div className="card-body">
+          {data ? (
+            <p>
+              Success! You may now head{""}
+              <Link to="/">back to homepage</Link>
+            </p>
+          ) : (
+            <form onSubmit={handleFormSubmit}>
+              <input
+                className="form-input"
+                placeholder="your email"
+                name="email"
+                type="email"
+                value={formState.email}
+                onChange={handleChange}
+              />
+              <input
+                className="form-input"
+                placeholder="*****"
+                name="password"
+                type="password"
+                value={formState.password}
+                onChange={handleChange}
+              />
+              <button
+                className="btn btn-block btn-primary"
+                style={{ cursor: "pointer" }}
+                type="submit"
+              >
+                Submit
+              </button>
+            </form>
+          )}
+
+          {error && (
+            <div className="my-3 p-3 bg-danger text-white">{error.message}</div>
+          )}
+        </div>
+      </div>
+    </main>
+  );
 };
