026fb868d8d1396cee28c21fc49208c6734e1105
James Geneser <jamesgeneser@gmail.com>
2023-04-18 16:49:18 -0600

    reverted login jsx to view locally hosted page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Form, Link } from "react-router-dom";
 import { useMutation } from "@apollo/client";
 import { LOGIN_USER } from "../utils/mutations";
+// import Button from "react-bootstrap/Button";
 
 import Auth from "../utils/auth";
 
@@ -38,49 +39,41 @@ const Login = (props) => {
   };
 
   return (
-    <main>
-      <div className="card">
-        <h4>Login</h4>
-        <div className="card-body">
-          {data ? (
-            <p>
-              Success! You may now head{""}
-              <Link to="/">back to homepage</Link>
-            </p>
-          ) : (
-            <form onSubmit={handleFormSubmit}>
-              <input
-                className="form-input"
-                placeholder="your email"
-                name="email"
-                type="email"
-                value={formState.email}
-                onChange={handleChange}
-              />
-              <input
-                className="form-input"
-                placeholder="*****"
-                name="password"
-                type="password"
-                value={formState.password}
-                onChange={handleChange}
-              />
-              <button
-                className="btn btn-block btn-primary"
-                style={{ cursor: "pointer" }}
-                type="submit"
-              >
-                Submit
-              </button>
-            </form>
-          )}
-
-          {error && (
-            <div className="my-3 p-3 bg-danger text-white">{error.message}</div>
-          )}
-        </div>
+    <>
+      <div className="d-flex flex-direction-column">
+        <form>
+          <input
+            className="form-input"
+            placeholder="your email"
+            name="email"
+            type="email"
+            value={formState.email}
+            onChange={handleChange}
+          />
+          <input
+            className="form-input"
+            placeholder="*****"
+            name="password"
+            type="password"
+            value={formState.password}
+            onChange={handleChange}
+          />
+          <button
+            className="btn btn-block btn-primary"
+            style={{ cursor: "pointer" }}
+            type="submit"
+          >
+            Submit
+          </button>
+        </form>
       </div>
-    </main>
+
+      {error && (
+        <div className="my-3 p-3 bg-danger text-white">{error.message}</div>
+      )}
+      {/* </div>
+      </div> */}
+    </>
   );
 };
 
