0ab6aa7eb6b608f1451168186bc4adc495d26972
James Geneser <jamesgeneser@gmail.com>
2023-04-19 12:35:26 -0600

    saving changes to signup

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -60,6 +60,19 @@ const Signup = () => {
           </Form.Group>
           <Form.Group
             className="mb-3"
+            controlId="formBasicEmail"
+            value={formState.email}
+            onChange={handleChange}
+            placeholder="your email"
+          >
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+          <Form.Group
+            className="mb-3"
             controlId="formBasicPassword"
             value={formState.password}
             name="password"
